01a0e3188f51889ba9d0762843fcbc8faf3e6b6a
Jeremy Zantua <jeremyzantua@gmail.com>
2020-03-13 10:58:42 +0800

    Added necessary libraries

--- RFIDPOS-2/RFIDPOS-2.ino
+++ RFIDPOS-2/RFIDPOS-2.ino
@@ -1,9 +1,15 @@
+#include <Wire.h>                   // for I2C communication with the LCD
+#include <LiquidCrystal_I2C.h>      // main library for the LCD functionality
+#include <SoftwareSerial.h>         // software Serial for communication with the GSM Module
+#include <EEPROM.h>                 // for allowing read/write operations to the EEPROM of the Arduino UNO
+#include <TimeLib.h>                // for time conversion functionality
+#include <SPI.h>                    // for SPI communication with the RFID scanner
+#include <MFRC522.h>                // main library for RFID scanner functionality
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
